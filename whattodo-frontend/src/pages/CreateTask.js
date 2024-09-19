10985a73685228f121c5af6ea7b60a214855148a
Abel Meseret <ameseret1990@gmail.com>
2024-09-19 19:29:34 +0300

    Add basic CreateTask component

---
+++ whattodo-frontend/src/pages/CreateTask.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CreateTask = () => {
+  return (
+    <div>
+      <h1>Create Task</h1>
+      {/* Task creation form will go here */}
+    </div>
+  );
+};
+
+export default CreateTask;
