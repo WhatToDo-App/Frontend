d24657ac75ed16aba0086354a595967cb6d5c4e7
Abel Meseret <ameseret1990@gmail.com>
2024-09-19 19:02:58 +0300

    Add basic TaskList component

---
+++ whattodo-frontend/src/pages/TaskList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const TaskList = () => {
+  return (
+    <div>
+      <h1>Task List</h1>
+      {/* Task list will go here */}
+    </div>
+  );
+};
+
+export default TaskList;
